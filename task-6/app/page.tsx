ecebba24f0ccae6128b1876dcbc94c99feab4a34
bemnet16 <bemnetadugnaw@gmail.com>
2024-08-10 12:07:36 +0300

    upload task-6 /job listing app/

--- task-6/app/page.tsx
+++ task-6/app/page.tsx
@@ -1,9 +1,17 @@
+import { ArrowBigRight } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="p-6 lg:w-3/4">
-      <Link href="/job-list">Job Lists</Link>
+    <div className="p-6 flex items-center justify-center h-screen w-full">
+      <Link
+        className="text-2xl flex hover:underline gap-4 text-orange-900"
+        href="/job-list"
+      >
+        Job Lists
+        <ArrowBigRight size={24} />
+      </Link>
     </div>
   );
 }
