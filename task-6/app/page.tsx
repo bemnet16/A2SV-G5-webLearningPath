79aa659b4964508b01a186bbed5335dc524df7a8
bemnet16 <bemnetadugnaw@gmail.com>
2024-08-10 14:25:12 +0300

    upload task-6 /job listing app/

--- task-6/app/page.tsx
+++ task-6/app/page.tsx
@@ -1,16 +1,15 @@
 import { ArrowBigRight } from "lucide-react";
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="p-6 flex items-center justify-center h-screen w-full">
+    <div className="p-6 flex bg-custom-bg bg-cover bg-center items-center justify-center h-screen w-full">
       <Link
-        className="text-2xl flex hover:underline gap-4 text-orange-900"
+        className="text-4xl flex hover:underline gap-4 mr-48 text-gray-700 font-bold"
         href="/job-list"
       >
         Job Lists
-        <ArrowBigRight size={24} />
+        <ArrowBigRight className="animate-ping" size={38} />
       </Link>
     </div>
   );
